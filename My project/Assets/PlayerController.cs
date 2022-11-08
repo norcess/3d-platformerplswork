058527a6e339a700da13dab937fa9821ad88bf7f
norcess <hwang2022@elam.co.uk>
2022-11-08 20:58:45 +0000

    control

--- My project/Assets/PlayerController.cs
+++ My project/Assets/PlayerController.cs
@@ -7,12 +7,25 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = GameObject.Find("Main Camera");
     }
 
-    // Update is called once per frame
+    float maxSpeed = 0.25f;
+    float rotation = 0.0f;
+    float camRotation = 0.0f;
+    float rotationSpeed = 2.0f;
+    float camRotationSpeed = 1.5f;
+    GameObject cam;
+
+
     void Update()
     {
-        
+        transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical"));
+
+        rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
+        transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
+
+        camRotation = camRotation + Input.GetAxis("Mouse Y") * camRotationSpeed;
+        cam.transform.localRotation = Quaternion.Euler(new Vector3(camRotation, 0.0f, 0.0f));
     }
 }
